d91524ab3ee0283dae042c06f3c598fd2cd16c67
kalq <bassam.saeed@gmail.com>
2015-10-21 13:05:32 -0400

    Massive redo, switched to wmutils no wm. Probably should have commit individually.

--- bash/.bash_profile
+++ bash/.bash_profile
@@ -4,7 +4,7 @@
 
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 
-PATH=$PATH:~/data/scripts
+PATH=$PATH:~/data/scripts:~/data/scripts/wmutils
 export PANEL_FIFO="/tmp/panel-fifo"
 
 [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
---
+++ scripts/wmutils/get_window.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# kalq - 2015 (c)
+# returns the window IDs of the windows that the mouse is hovering over
+
+mouse_x=$(wmp | cut -d' ' -f1)
+mouse_y=$(wmp | cut -d' ' -f2)
+
+for i in $(lsw | sed '1!G;h;$!d');
+do
+    win_x=$(wattr x $i)
+    win_y=$(wattr y $i)
+    win_width=$(wattr w $i)
+    win_height=$(wattr h $i)
+
+    if [ "$mouse_x" -ge "$win_x" -a "$mouse_x" -le $((win_x + win_width)) ] ; then
+        if [ "$mouse_y" -ge "$win_y" -a "$mouse_y" -le $((win_y + win_height)) ] ; then
+            echo $i
+        fi
+    fi
+done
