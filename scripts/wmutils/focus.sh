3b91472b74c2ddc0e59bf9d8622c06a54073fd22
kalq <bassam.saeed@gmail.com>
2015-10-21 17:48:43 -0400

    finalized colours. for now

--- scripts/wmutils/focus.sh
+++ scripts/wmutils/focus.sh
@@ -4,8 +4,8 @@
 # window focus wrapper that sets borders and can focus next/previous window
 
 BW=${BW:-2}                    # border width
-ACTIVE=${ACTIVE:-0xffffff}     # active border color
-INACTIVE=${INACTIVE:-0x333333} # inactive border color
+ACTIVE=${ACTIVE:-0xfd7d4a}     # active border color
+INACTIVE=${INACTIVE:-0xc8283e} # inactive border color
 
 # get current window id
 CUR=$(pfw)
