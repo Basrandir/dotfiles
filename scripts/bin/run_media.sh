dfc3d3becdc2ee96d65915c83be43b37a9d3bcd9
kalq <bassam.saeed@gmail.com>
2017-03-16 10:15:28 -0400

    moved media rofi command to seperate script

---
+++ scripts/bin/run_media.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+DIR=$HOME/media/
+unset files; declare -a files
+unset names; declare -a names
+while IFS= read -r -u3 -d $'\0' file; do
+    files+=( "$file" )        # or however you want to process each file
+    fn=$(basename "${file}")
+    names+=( "$fn" )
+done 3< <(find "${DIR}" -type f -print0)
+
+
+function lists()
+{
+    OLDIFS=${IFS}
+    IFS='|'
+    for n in ${names[@]}
+    do
+        echo ${n}
+    done
+    IFS=${OLDIFS}
+
+}
+ID=$(lists | rofi -dmenu -format "i" -i)
+
+if [ -n ${ID} ]
+then
+    xdg-open "${files[${ID}]}"
+fi
