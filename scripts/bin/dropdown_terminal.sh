05997974a3fe5ae8b5a986dcc343403f3b4f23be
kalq <bassam.saeed@gmail.com>
2017-03-28 08:47:33 -0400

    added quotes to prevent globbing and word splitting

--- scripts/bin/dropdown_terminal.sh
+++ scripts/bin/dropdown_terminal.sh
@@ -2,7 +2,7 @@
 
 terminal="st"
 
-if [ -z $(xdotool search --classname "dropdown") ]; then
+if [ -z "$(xdotool search --classname 'dropdown')" ]; then
     bspc rule -a $terminal:dropdown state=floating hidden=on border=off sticky=on
 
     # Use of xtoolwait is recommended instead of the until-loop.
@@ -10,7 +10,7 @@ if [ -z $(xdotool search --classname "dropdown") ]; then
     # mapped to a window before continuing the script.
     # xtoolwait, as the name implies, does not work with wayland.
     #xtoolwait "$terminal" -c "$terminal" -n dropdown
-    ($terminal -c $terminal -n dropdown &); until [ $(bspc query -N -n .hidden | tail -n1) ]; do :; done
+    ($terminal -c $terminal -n dropdown &); until [ "$(bspc query -N -n .hidden | tail -n1)" ]; do :; done
 
     # find the newest hidden terminal
     wid=$(bspc query -N -n .hidden | tail -n1)
@@ -18,18 +18,18 @@ if [ -z $(xdotool search --classname "dropdown") ]; then
     width=$(bspc query -T -m | grep -oE "[0-9]{0,4}" | sed -n '16 p')
     height=$(bspc query -T -m | grep -oE "[0-9]{0,4}" | sed -n '17 p')
 
-    xdotool windowmove $wid 0 0
-    xdotool windowsize $wid $(expr $width) $(expr $height / 3)
+    xdotool windowmove "$wid" 0 0
+    xdotool windowsize "$wid" "$(expr $width)" "$(expr $height / 3)"
 
-    compton-trans -w $wid -o 90
+    compton-trans -w "$wid" -o 90
 
     # Reveal the terminal once all the geometric changes are complete
-    bspc node $wid -g hidden=off -f
+    bspc node "$wid" -g hidden=off -f
 
     bspc rule -r $terminal:dropdown
 else
-    wid=$(printf "0x%x" $(xdotool search --classname "dropdown"))
+    wid=$(printf "0x%x" "$(xdotool search --classname "dropdown")")
 
     # toggle visibility of dropdown menu
-    bspc node $wid -g hidden -f
+    bspc node "$wid" -g hidden -f
 fi
