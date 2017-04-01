344feabb0eb06b3a8b3f781cd3a0eb2da59146a1
kalq <bassam.saeed@gmail.com>
2017-04-01 12:39:06 -0400

    initial commit

---
+++ emacs/.emacs.d/elpa/xresources-theme-20160331.702/xresources-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "xresources-theme" "20160331.702" "Use your .Xresources as your emacs theme" 'nil :keywords '("xresources" "theme"))
---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,60 @@
+(package-initialize)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror t)
+
+;; PACKAGE MANAGEMENT
+(require 'package)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
+
+;; install use-package if not installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode t))
+
+(use-package xresources-theme
+  :ensure t
+  :config
+  (load-theme 'xresources t))
+
+;; USER INTERFACE
+;; line numbers
+(linum-mode)
+
+(column-number-mode t)
+
+;; remove clutter
+(setq inhibit-splash-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+(when (boundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+
+;; BEHAVIOUR
+;; automatically insert matching parens
+(add-hook 'emacs-list-mode-hook (lambda () (electric-pair-mode -1)))
+(electric-pair-mode 1)
+
+;; highlight matching parens
+(show-paren-mode t)
+
+;; always follow symlinks
+(setq vc-follow-symlinks t)
+
+;; auto revert files on change
+(global-auto-revert-mode t)
+
+;; display current time
+(display-time-mode t)
+
+;; do no create backup files
+(setq make-backup-files nil)
