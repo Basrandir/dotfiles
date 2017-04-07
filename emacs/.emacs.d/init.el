5f2b3c0547557b7ae5f92b116f6ffdad13a5d663
kalq <bassam.saeed@gmail.com>
2017-04-07 16:26:25 -0400

    Org-mode keybindings

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -35,6 +35,16 @@
   :config
   (load-theme 'zenburn t))
 
+;; ORG MODE
+;; keybindings
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(define-key global-map "\C-cc" 'org-capture)
+(define-key global-map "\C-cb" 'org-iswitchb)
+
+(setq org-agenda-files (list "~/doc/org/work.org"
+			     "~/doc/org/home.org"))
+
 ;; USER INTERFACE
 ;; show column number in modeline
 (column-number-mode t)
