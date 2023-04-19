680f6f16d565bfb2166b7284fd1855293a04365b
Bassam Saeed <bassam.saeed@gmail.com>
2023-04-19 19:33:50 -0400

    Dired code to toggle showing dotfiles
    I'm not entirely happy with the workflow right now. I often go into a
    directory and then coming back resets the toggle which is annoying.

--- files/emacs/modules/module-dired.el
+++ files/emacs/modules/module-dired.el
@@ -7,6 +7,7 @@
 (use-package dired
   :bind (:map dired-mode-map
 	      ("C-c o" . 'dired-open-file)
+	      ("C-c d" . 'dired-dotfiles-toggle)
 	      ;; by default the binding for mouse-2 is
 	      ;; 'dired-mouse-find-file-other-window
 	      ([mouse-2] . 'dired-mouse-find-file))
@@ -15,11 +16,25 @@
   (dired-listing-switches "-lha --group-directories-first")
   ;; Stops the proliferation of dired buffers
   (dired-kill-when-opening-new-dired-buffer t)
-  :config
+  :init
   (defun dired-open-file ()
     "Open current file in default application."
     (interactive)
-    (start-process "xdg-open-in-dired" nil "xdg-open" (dired-get-filename))))
+    (start-process "xdg-open-in-dired" nil "xdg-open" (dired-get-filename)))
+
+  ;; Taken from https://www.emacswiki.org/emacs/DiredOmitMode
+  (defun dired-dotfiles-toggle ()
+    "Show/hide dot-files"
+    (interactive)
+    (when (equal major-mode 'dired-mode)
+      (if (or (not (boundp 'dired-dotfiles-show-p)) dired-dotfiles-show-p) ; if currently showing
+	  (progn 
+	    (set (make-local-variable 'dired-dotfiles-show-p) nil)
+	    (message "h")
+	    (dired-mark-files-regexp "^\\\.")
+	    (dired-do-kill-lines))
+	(progn (revert-buffer) ; otherwise just revert to re-show
+	       (set (make-local-variable 'dired-dotfiles-show-p) t))))))
 
 (use-package diredfl
   :after dired
@@ -27,5 +42,6 @@
   (diredfl-global-mode 1))
 
 (provide 'module-dired)
+
 
 ;;; module-dired.el ends here
