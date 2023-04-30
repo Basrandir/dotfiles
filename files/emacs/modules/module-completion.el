eff211b9a437869d5f56e8a1e2f2ccbed42c4c68
Bassam Saeed <bassam.saeed@gmail.com>
2023-04-30 13:16:20 -0400

    module-completion.el: Update setup
    Add additional keybindings for consult and expand corfu usability.

--- files/emacs/modules/module-completion.el
+++ files/emacs/modules/module-completion.el
@@ -4,17 +4,39 @@
 
 ;;; Code:
 
+(defun consult-emoji ()
+  "Insert an emoji at point."
+  (interactive)
+  (let* ((emojis (with-temp-buffer))
+	 (selected-emoji (consult--read
+                          emojis
+                          :prompt "Emoji: "
+                          :category 'emoji
+                          :sort nil
+                          :history t)))
+    (insert selected-emoji)))
+
 (use-package consult
   :bind (
-	 ("C-s" . consult-line)
+	 ("C-s" . consult-line)        ;; orig. isearch-forward
+	 ("C-x b" . consult-buffer)    ;; org. switch-to-buffer
 	 ("M-s d" . consult-find)
+	 ("M-s f" . (lambda () (interactive) (consult-find "~")))
 	 ("M-s r" . consult-ripgrep))
   :config
+  ;; Exclude mnt directory from consult-find
+  (setq consult-find-args "find . -not ( -wholename */.* -prune -o -name mnt -prune )")
   (consult-customize
    consult-find :preview-key 'any))
 
 (use-package corfu
-  :init (global-corfu-mode)
+  :init
+  (global-corfu-mode)
+  (corfu-popupinfo-mode)
+  :bind (:map corfu-map
+	      ("M-p" . #'corfu-popupinfo-scroll-down)
+	      ("M-n" . #'corfu-popupinfo-scroll-up)
+	      ("M-d" . #'corfu-popupinfo-toggle))
   :custom
   (corfu-auto t)
   :config
@@ -23,6 +45,13 @@
     (when (where-is-internal #'completion-at-point (list (current-local-map)))
       (corfu-mode 1)))
   (add-hook 'minibuffer-setup-hook #'corfu-enable-in-minibuffer))
+
+;; (use-package kind-icon
+;;   :after corfu
+;;   :custom
+;;   (kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
+;;   :config
+;;   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
 (use-package embark
   :bind
