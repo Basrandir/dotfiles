c4ff93e1140548205c2e249eb39a34a9b0b19111
Bassam Saeed <bassam.saeed@gmail.com>
2023-04-30 13:17:39 -0400

    disable line highlight for eat terminal, similar to how it is for vterm

--- files/emacs/modules/module-shell.el
+++ files/emacs/modules/module-shell.el
@@ -8,7 +8,9 @@
   :init
   (add-hook 'eat-mode-hook #'hide-mode-line-mode)
   :custom
-  (explicit-shell-file-name "/home/bassam/.guix-home/profile/bin/fish"))
+  (explicit-shell-file-name "/home/bassam/.guix-home/profile/bin/fish")
+  :hook (eat-mode . (lambda()
+		      (setq-local global-hl-line-mode nil))))
 
 ;; An alternative Emacs terminal built on top libvterm. Need to figure
 ;; out a way to automate colours.
