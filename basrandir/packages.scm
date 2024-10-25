39caf01dabb0949114df91e6768eff60836a9976
Bassam Saeed <github@bassamsaeed.ca>
2024-10-25 08:44:21 -0400

    packages.scm: Remove personal river package config
    River package in Guix repo now works properly

--- basrandir/packages.scm
+++ basrandir/packages.scm
@@ -12,43 +12,6 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages))
 
-(define-public river
-  (package
-    (name "river")
-    (version "0.2.4")
-    (source (origin
-	      (method git-fetch)
-	      (uri (git-reference
-		    (url "https://github.com/riverwm/river.git")
-		    (commit (string-append "v" version))
-		    (recursive? #t)))
-	      (file-name (git-file-name name version))
-	      (sha256
-	       (base32
-		"1nvhqs6wwisf8ama7y1y3q3nf2jm9sh5bn46z8kyds8cikm0x1vh"))))
-    (build-system gnu-build-system)
-    (inputs (list libevdev libxkbcommon pixman pkg-config scdoc wayland wayland-protocols wlroots-0.16 zig))
-    (arguments
-     (list #:phases #~(modify-phases %standard-phases
-			(delete 'configure)
-			(replace 'build
-			  (lambda* (#:key outputs #:allow-other-keys)
-			    (let ((out (assoc-ref outputs "out")))
-			      (setenv "ZIG_GLOBAL_CACHE_DIR" (string-append (getcwd) "/zig-cache"))
-			      (invoke "zig" "build" "-Drelease-safe" "-Dxwayland" "--prefix" out "install"))))
-			(add-after 'build 'create-desktop-file
-			  (lambda* (#:key outputs #:allow-other-keys)
-			    (let* ((out (assoc-ref outputs "out"))
-				   (wayland-sessions (string-append out "/share/wayland-sessions")))
-			      (mkdir-p wayland-sessions)
-			      (copy-file "contrib/river.desktop" (string-append wayland-sessions "/river.desktop")))))
-			(delete 'check)
-			(delete 'install))))
-    (home-page "https://github.com/riverwm/river")
-    (synopsis "Dynamic tiling wayland compositor")
-    (description "River is a dynamic tiling wayland compositor.")
-    (license license:expat)))
-
 (define-public xdg-desktop-portal
   (package
     (name "xdg-desktop-portal")
